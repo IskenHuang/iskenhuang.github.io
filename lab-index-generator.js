b5ffddb643f35dc3db63824cc9bb019fbc83c2b1
Isken Huang <iskenhuang@gmail.com>
2016-03-01 13:46:31 +0800

    update

--- lab-index-generator.js
+++ lab-index-generator.js
@@ -3,19 +3,24 @@ var FS = require('fs')
 // create lab index
 const labFolderName = 'lab'
 const labName = 'Lab index'
-const templateItem = '<h3><a href="/lab/{{name}}">{{name}}</a></h3>'
+const templateItem = '<h3><a href="/' + labFolderName + '/{{name}}">{{name}}</a></h3>'
 const templateName = 'template.html'
+const indexName = 'index.html'
 const labFolder = FS.readdirSync(labFolderName)
 var labs = []
-var bodyTemplate = '<div class="cell-inner">'
+var bodyTemplate = '<style>h3 {padding: 0.8rem 0;} h3:nth-child(2n) { background-color: #fafafa;}</style><div class="cell-inner row borderbox">'
 
 const normalHtml = FS.readFileSync(templateName).toString()
 var result = ''
 
 labFolder.map(function(item) {
     if(item.indexOf('.') < 0) {
-        bodyTemplate += templateItem.replace(/\{\{name\}\}/g, item)
-        labs.push(item)
+        const _name = [labFolderName, item, indexName].join('/')
+        const hasIndex = FS.readFileSync(_name)
+        if(hasIndex) {
+            bodyTemplate += templateItem.replace(/\{\{name\}\}/g, item)
+            labs.push(item)
+        }
     }
 })
 
@@ -24,4 +29,4 @@ bodyTemplate += '</div>'
 result = normalHtml.replace(/\{\{title\}\}/g, labName)
                         .replace(/\{\{body\}\}/g, bodyTemplate)
 
-FS.writeFileSync('lab/index.html', result)
+FS.writeFileSync([labFolderName, indexName].join('/'), result)
