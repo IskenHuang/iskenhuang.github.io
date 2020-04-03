53b06db3eb57c2ed3a4c142c6b75d68b9c187171
Isken Huang <iskenhuang@gmail.com>
2020-04-03 21:49:37 +0800

    update build script

--- scripts/lab-index-generator.js
+++ scripts/lab-index-generator.js
@@ -3,14 +3,24 @@ var FS = require('fs')
 // create lab index
 const labFolderName = 'lab'
 const labName = 'Lab index'
-const templateItem = '<h3><a href="/' + labFolderName + '/{{name}}">{{name}}</a></h3>'
-const templateName = __dirname + '/template.html'
+const templateItem = `<h3><a href="/${labFolderName}/{{name}}">{{name}}</a></h3>`
+const templateName = `${__dirname}/template.html`
 const indexName = 'index.html'
 const labFolder = FS.readdirSync(labFolderName)
 var labs = []
-var bodyTemplate = '<style>h3 {padding: 1.5vw 2vw; box-sizing: border-box;} h3:nth-child(2n) { background-color: #fafafa; }</style><div class="row fd-col">'
+var bodyTemplate = removeLineBreak(`
+<style>
+    h3 {padding: 1.5vw 2vw; box-sizing: border-box; margin: 0; }
+    h3:nth-child(2n) { background-color: #eee; }
+    @media (prefers-color-scheme: dark) {
+        h3:nth-child(2n) { background-color: #666; }
+    }
+</style>
+<div class="row fd-col">
+`)
 
 const normalHtml = FS.readFileSync(templateName).toString()
+const commonCss = removeLineBreak(FS.readFileSync(`${__dirname}/../common.css`).toString())
 var result = ''
 
 labFolder.map(function(item) {
@@ -28,8 +38,14 @@ labFolder.map(function(item) {
 bodyTemplate += '</div>'
 
 result = normalHtml.replace(/\{\{title\}\}/g, labName)
+                        .replace(/\{\{commoncss\}\}/g, commonCss)
                         .replace(/\{\{body\}\}/g, bodyTemplate)
 
 FS.writeFileSync([labFolderName, indexName].join('/'), result)
 
 console.log('build success')
+
+// -------------------------------------
+function removeLineBreak(s) {
+    return s.replace(/\n/g, '')
+}
